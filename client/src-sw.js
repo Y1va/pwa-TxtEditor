1f277b4adaf755b37b86b1a99562e77b96b82d4a
ZeuS <ponzeadrian@gmail.com>
2024-04-16 14:36:45 +1000

    Implemented asset caching for service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -11,20 +11,36 @@ const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
     new CacheableResponsePlugin({
-      statuses: [0, 200],
+      statuses: [0, 200]
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
+      maxAgeSeconds: 30 * 24 * 60 * 60
+    })
+  ]
 });
 
 warmStrategyCache({
   urls: ['/index.html', '/'],
-  strategy: pageCache,
+  strategy: pageCache
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+
+// Register a route handler for caching assets (stylesheets, scripts, workers)
+registerRoute(
+  // Match requests with style, script or worker destination
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  // Use Stale-While-Revalidate caching strategy
+  new StaleWhileRevalidate({
+    // Name of the cache for assets
+    cacheName: 'asset-cache',
+    plugins: [
+      // Plugin to configure cachable responses
+      new CacheableResponsePlugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
