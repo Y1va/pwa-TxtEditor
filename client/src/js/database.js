81b249867464c74d015e2e8c9f1bca034ba04f34
ZeuS <ponzeadrian@gmail.com>
2024-04-16 15:36:08 +1000

    Added function to add some content to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -9,11 +9,26 @@ const initdb = async () =>
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
-    },
+    }
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+// Function to add some content to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+  console.log('PUT from the database');
+
+  // Open the database
+  const contactDb = await openDB('jate', 1);
+
+  // Create new transaction
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  // Adds the data to the object store
+  const request = store.put({ id: 1, value: content })
+  const result = await request;
+  console.log('Data saved to the database', result)
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
